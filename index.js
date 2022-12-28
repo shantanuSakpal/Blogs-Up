30f9686f0861cdde041441eede10c47f6ddbe358
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2022-12-28 12:09:19 +0530

    pakagejson modifications

--- index.js
+++ index.js
@@ -60,7 +60,5 @@ app.get("/posts/:url", (req, res) => {
 })
 
 
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server started on  ", PORT));
